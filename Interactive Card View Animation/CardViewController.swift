25514ea4ab3a802d9996aae904489fefc7cfb28a
Mikle <beardmikle@gmail.com>
2023-01-30 22:19:55 +0300

    Add files via upload

---
+++ Interactive Card View Animation/CardViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CardViewController.swift
+//  Interactive Card View Animation
+//
+//  Created by beardmikle on 28.01.2023.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+    
+    @IBOutlet weak var handleArea: UIView!
+}
+
